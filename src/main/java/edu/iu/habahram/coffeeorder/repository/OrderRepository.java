916022b00e49e1345b33202b290aefed4f1e32f5
Mallory Miller <mallorysilvermiller@gmail.com>
2024-02-12 09:12:50 -0500

    init

--- src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
+++ src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
@@ -11,6 +11,12 @@ public class OrderRepository {
             case "dark roast":
                 beverage = new DarkRoast();
                 break;
+            case "espresso":
+                beverage = new Espresso();
+                break;
+            case "house blend":
+                beverage = new HouseBlend();
+                break;
         }
         if (beverage == null) {
             throw new Exception("Beverage type '%s' is not valid!".formatted(order.beverage()));
@@ -20,6 +26,12 @@ public class OrderRepository {
                 case "milk":
                    beverage = new Milk(beverage);
                    break;
+                case "soy":
+                    beverage = new Soy(beverage);
+                    break;
+                case "whip":
+                    beverage = new Whip(beverage);
+                    break;
                 case "mocha":
                     beverage = new Mocha(beverage);
                     break;
